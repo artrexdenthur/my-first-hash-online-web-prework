763b7654599246d37e91a294a63bae7be0da0acb
Paul Ashour <paul.ashour@gmail.com>
2018-10-05 21:43:57 +0000

    Done.

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,16 +1,19 @@
-def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
-  
+def my_hash()
+  # uses the literal constructor to create a hash that contains key/value pairs
+  shipping_manifest = { 'anything' => 'something' }
 end
 
-
-def shipping_manifest
-  # set a variable called `the_manifest`, equal to a hash
-  # fill that hash with key/value pairs that describe the following information: 
-  # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
-
-
+def shipping_manifest()
+  # returns a hash with key/value pairs describing old-timey items
+  # 2 whale bone corsets
+  # 5 oil paintings
+  # 3 porcelain vases
+  # items are keys, numbers are values
+  shipping_manifest = {
+    'whale bone corsets' => 2,
+    'oil paintings' => 5,
+    'porcelain vases' => 3
+  }
 end
 
 def retrieval
@@ -20,8 +23,10 @@ def retrieval
                   "oil paintings" => 3
                  }
 
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
+  # operates on shipping_manifest hash to return the value of the
+  # 'oil paintings' key
+  
+  shipping_manifest['oil paintings']
 end
 
 def adding
@@ -30,14 +35,18 @@ def adding
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-  # add 2 muskets to the shipping_manifest hash below
-
-
-
-  # add 4 gun powder to the shipping_manifest hash below
-
-
-
-  # return the shipping_manifest hash below
-
+  # operates on the shipping_manifest hash to add a key/value pair
+  shipping_manifest['muskets'] = 2
+  shipping_manifest['gun powder'] = 4
+  shipping_manifest
 end
+
+def new()
+  new = 'thing'
+  indent = 'please indent'
+  indent.characters.each do |character|
+    character = 'thanks'
+  end
+end
+
+    
