1f54dfdab9427043c4a5939cb1257df09bff9761
Paul Ashour <paul.ashour@gmail.com>
2018-10-05 21:41:25 +0000

    automatically backed up by learn

---
+++ my_first_hash.rb
@@ -0,0 +1,40 @@
+def my_hash()
+  # uses the literal constructor to create a hash that contains key/value pairs
+  shipping_manifest = { 'anything' => 'something' }
+end
+
+def shipping_manifest()
+  # returns a hash with key/value pairs describing old-timey items
+  # 2 whale bone corsets
+  # 5 oil paintings
+  # 3 porcelain vases
+  # items are keys, numbers are values
+  shipping_manifest = {
+    'whale bone corsets' => 2,
+    'oil paintings' => 5,
+    'porcelain vases' => 3
+  }
+end
+
+def retrieval(manifest = shipping_manifest)
+  # operates on shipping_manifest hash to return the value of the
+  # 'oil paintings' key
+  manifest['oil paintings']
+end
+
+def adding(manifest = shipping_manifest)
+  # operates on the shipping_manifest hash to add a key/value pair
+  manifest['muskets'] = 2
+  manifest['gun powder'] = 4
+  manifest
+end
+
+def new()
+  new = 'thing'
+  indent = 'please indent'
+  indent.characters.each do |character|
+    character = 'thanks'
+  end
+end
+
+    
